9a3e2942c80deee01462f18a8b0e7203faa3efc7
pedro.mendes <pedro.mendes@corp.globo.com>
2018-10-05 11:06:12 -0300

    fix: assign state values to inputs and vice-versa.

--- src/components/DebuggerForm.js
+++ src/components/DebuggerForm.js
@@ -11,7 +11,7 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 
 class DebuggerForm extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             ...props.form
@@ -20,38 +20,36 @@ class DebuggerForm extends Component {
 
     updateState = (evnt) => {
         let target = evnt.target.name;
-        let value = target.type === 'checkbox' ? target.checked : target.value;
-        console.log(evnt.target, target, value);
+        let value = evnt.target.type === 'checkbox' ? evnt.target.checked : evnt.target.value;
         this.setState({ [target]: value });
     }
 
     submit = (evnt) => {
         evnt.preventDefault();
         evnt.stopPropagation();
-        console.log(this.state);
         return false;
     }
 
-    render() {
+    render = () => {
         return (
             <form onSubmit={this.submit}>
                 <div>
-                    <TextField name="auth_uri" label="Authorize URI (required)" className="fluid" onChange={this.updateState} />
+                    <TextField name="auth_uri" label="Authorize URI (required)" className="fluid" value={this.state.auth_uri} onChange={this.updateState} />
                 </div>
                 <div>
-                    <TextField name="redirect_uri" label="Redirect URI (required)" className="fluid" onChange={this.updateState} />
+                    <TextField name="redirect_uri" label="Redirect URI (required)" className="fluid" value={this.state.redirect_uri} onChange={this.updateState} />
                 </div>
                 <div>
-                    <TextField name="client_id" label="Client ID (required)" className="fluid" onChange={this.updateState} />
+                    <TextField name="client_id" label="Client ID (required)" className="fluid" value={this.state.client_id} onChange={this.updateState} />
                 </div>
                 <div>
-                    <TextField name="scope" label="Scope (required)" className="fluid" onChange={this.updateState} />
+                    <TextField name="scope" label="Scope (required)" className="fluid" value={this.state.scope} onChange={this.updateState} />
                 </div>
                 <div>
-                    <TextField name="state" label="State" className="fluid" onChange={this.updateState}/>
+                    <TextField name="state" label="State" className="fluid" value={this.state.state} onChange={this.updateState} />
                 </div>
                 <div>
-                    <TextField name="nonce" label="Nonce" className="fluid" onChange={this.updateState}/>
+                    <TextField name="nonce" label="Nonce" className="fluid" value={this.state.nonce} onChange={this.updateState} />
                 </div>
 
                 <FormControl component="fieldset">
@@ -59,32 +57,30 @@ class DebuggerForm extends Component {
                     <FormGroup>
                         <div> Type </div>
                         <FormControlLabel
-                            control={<Checkbox name="reponse_type_code" value="code" color="primary" />}
+                            control={<Checkbox name="reponse_type_code" value="code" color="primary" checked={this.state.reponse_type_code} onChange={this.updateState} />}
                             label="code" />
 
                         <FormControlLabel
-                            control={<Checkbox name="reponse_type_token" value="token" color="primary" />}
+                            control={<Checkbox name="reponse_type_token" value="token" color="primary" checked={this.state.reponse_type_token} onChange={this.updateState} />}
                             label="token" />
 
                         <FormControlLabel
-                            control={<Checkbox name="reponse_type_idtoken" value="id_token" color="primary" />}
+                            control={<Checkbox name="reponse_type_idtoken" value="id_token" color="primary" checked={this.state.reponse_type_idtoken} onChange={this.updateState} />}
                             label="id_token" />
                     </FormGroup>
 
-                    <RadioGroup>
-                        <div> Mode (required) </div>
-                        <FormControlLabel
-                            control={<Radio name="reponse_mode" value="query" color="primary" onChange={this.updateState} />}
-                            label="query" />
-
-                        <FormControlLabel
-                            control={<Radio value="reponse_mode" value="form_post" color="primary" onChange={this.updateState} />}
-                            label="form_post" />
-
-                        <FormControlLabel
-                            control={<Radio value="reponse_mode" value="fragment" color="primary" onChange={this.updateState} />}
-                            label="fragment" />
-                    </RadioGroup>
+                    <FormControl component="fieldset">
+                        <FormLabel component="legend">Mode (required)</FormLabel>
+                        <RadioGroup
+                            aria-label="Mode"
+                            name="reponse_mode"
+                            onChange={this.updateState}
+                            value={this.state.reponse_mode}>
+                            <FormControlLabel control={<Radio />} label="query" value="query" />
+                            <FormControlLabel control={<Radio />} label="form_post" value="form_post" />
+                            <FormControlLabel control={<Radio />} label="fragment" value="fragment" />
+                        </RadioGroup>
+                    </FormControl>
 
                     <Button variant="contained" type="submit">
                         Submit
