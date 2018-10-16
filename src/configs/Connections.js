61d969658d137dc42b576c42b5d958e0b64c6e9a
pedro.mendes <pedro.mendes@corp.globo.com>
2018-10-16 18:10:32 -0300

    feature: isolate pre-configurated endpoints to a config/Connection file

---
+++ src/configs/Connections.js
@@ -0,0 +1,15 @@
+export default Connections = [
+    {
+        "name": "GloboID DEV",
+        "connection": {
+            authority: '',
+            client_id: '',
+            redirect_uri: '',
+            post_logout_redirect_uri: '',
+            response_type: '',
+            scope: '',
+            filterProtocolClaims: true,
+            loadUserInfo: true
+        }
+    }
+]; 
--- src/utils/OIDC.js
+++ src/utils/OIDC.js
@@ -1,24 +1,11 @@
-
-import * as Oidc from 'oidc-client';
+import * as Oidc from 'oidc-client'
+import Connections from '../configs/Connections'
 
 class OIDC {
     connect = () => {
         Oidc.Log.logger = console;
         Oidc.Log.level = Oidc.Log.INFO;
-
-        var settings = {
-            authority: 'https://globoid-connect.be.dev.globoi.com/auth/realms/globo.com',
-            client_id: 'client-dev',
-            redirect_uri: 'http://localhost:8080/redirect',
-            post_logout_redirect_uri: 'http://localhost:8080/logout',
-            response_type: 'id_token token',
-            scope: 'openid tester',
-
-            filterProtocolClaims: true,
-            loadUserInfo: true
-        };
-
-        var client = new Oidc.OidcClient(settings);
+        var client = new Oidc.OidcClient(Connections[0].connection);
 
         client.createSigninRequest({ state: { bar: 15 } }).then(function(req) {
             console.log("signin request", req.url, req);
