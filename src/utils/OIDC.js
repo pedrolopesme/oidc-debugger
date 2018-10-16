60946094254dfc42784c460697be4f7869e5435f
pedro.mendes <pedro.mendes@corp.globo.com>
2018-10-16 09:50:53 -0300

    minor: change address in return callback

--- src/utils/OIDC.js
+++ src/utils/OIDC.js
@@ -9,8 +9,8 @@ class OIDC {
         var settings = {
             authority: 'https://globoid-connect.be.dev.globoi.com/auth/realms/globo.com',
             client_id: 'client-dev',
-            redirect_uri: 'http://localhost:3000/redirect',
-            post_logout_redirect_uri: 'http://localhost:3000/logout',
+            redirect_uri: 'http://localhost:8080/redirect',
+            post_logout_redirect_uri: 'http://localhost:8080/logout',
             response_type: 'id_token token',
             scope: 'openid tester',
 
