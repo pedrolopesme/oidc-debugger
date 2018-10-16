7a9f19c977857b393ab19812ab5d7b77b62a81fb
pedro.mendes <pedro.mendes@corp.globo.com>
2018-10-16 09:50:32 -0300

    feature: output user data returned from OIDC flow

--- src/containers/RedirectContainer.js
+++ src/containers/RedirectContainer.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import * as Oidc from 'oidc-client';
 
 class RedirectContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: undefined
+        }
+    }
+
+    componentDidMount= () => {
+        let instance = this;
+        new Oidc.UserManager().signinRedirectCallback()
+        .then(function(user) {
+            instance.setState({ user: user });
+        }).catch(function(err) {
+            console.error(err);
+        });
+    }
+
     render() {
         return (
         <div className="redirect-container">
-            Redirected
+            <h3> User Data </h3>
+            <div><pre> {this.state.user && (
+                JSON.stringify(this.state.user, null, 2) 
+            )}</pre></div>
         </div>)
     }
 }
