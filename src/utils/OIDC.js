b87e8ecfc5149b5ac1a070c723ef3fcf5eab8ec6
pedro.mendes <pedro.mendes@corp.globo.com>
2019-11-07 16:14:40 -0300

    fix: return Integration Type properly

--- src/utils/OIDC.js
+++ src/utils/OIDC.js
@@ -28,9 +28,11 @@ export class OIDC {
  * response types. 
  */
 export function IntegrationType(code=false, token=false, idtoken=false){
+    console.log(`idtoken=${idtoken}, code=${code}, token=${token}`);
+    if (!idtoken && !code && !token) return "";
     if (idtoken && !code && !token) return IMPLICITY;
-    if (!idtoken && code && !token) return IMPLICITY;
-    if (!idtoken && code && token)  return IMPLICITY;
+    if (!idtoken && !code && token)  return IMPLICITY;
+    if (idtoken && !code && token)  return IMPLICITY;
     if (!idtoken && code && !token) return AUTH_CODE;
     return HYBRID;
 };
