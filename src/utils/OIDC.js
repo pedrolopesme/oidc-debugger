195861026a24e966ade55e2420a50e461a1ad427
pedro.mendes <pedro.mendes@corp.globo.com>
2019-11-07 15:47:52 -0300

    minor: improve OIDC util.
    Add IntegrationType func and comments

--- src/utils/OIDC.js
+++ src/utils/OIDC.js
@@ -1,21 +1,36 @@
 import * as Oidc from 'oidc-client'
 
-// TODO refactor
-class OIDC {
+// Integration Types
+const AUTH_CODE = "Authorization Code Flow",
+      HYBRID = "Hybrid Flow",
+      IMPLICITY = "Implicit Flow";
+
+
+/**
+ * OIDC client lib wrapper
+ */
+export class OIDC {
     connect = (connection) => {
         Oidc.Log.logger = console;
         Oidc.Log.level = Oidc.Log.INFO;
         var client = new Oidc.OidcClient(connection);
 
         client.createSigninRequest({ state: { bar: 15 } }).then(function(req) {
-            console.log("signin request", req.url, req);
             window.location = req.url;
         }).catch(function(err) {
             console.error(err);
         });
-
     }
+};
 
-}
-
-export default OIDC;
+/**
+ * Calculates the type of integration (implicity, hybrid, auth code) based on 
+ * response types. 
+ */
+export function IntegrationType(code=false, token=false, idtoken=false){
+    if (idtoken && !code && !token) return IMPLICITY;
+    if (!idtoken && code && !token) return IMPLICITY;
+    if (!idtoken && code && token)  return IMPLICITY;
+    if (!idtoken && code && !token) return AUTH_CODE;
+    return HYBRID;
+};
